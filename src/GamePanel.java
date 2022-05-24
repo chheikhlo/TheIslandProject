0605787cded3787bc2724ba1b6b7be375700a7ea
Medmat0 <yassineelmatrour@gmail.com>
2022-05-24 11:03:07 +0200

    new hexagone

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,60 +1,164 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+// add mouse motion listener
+public class GamePanel extends JPanel implements  Runnable, MouseListener , MouseMotionListener {
+    // reglage d'ecran
+    final int Tules = 20; // chaque tules a 20*20px
+    final int scale = 3;
 
-public class GamePanel extends JPanel implements  Runnable  {
-     // reglage d'ecran
-     final int  Tules =  20; // chaque tules a 20*20px
-     final int scale = 3;
-     final int Tulestaill = Tules*scale; // 60px dans le plateau width
-     final int TullesLarg = Tulestaill +10; // 70px height
-     final int MaxScreenCol = 14;
-     final int MaxScreenRow = 11;
-     final int ScreenWidth = Tulestaill*MaxScreenCol; // 960px width
-     final int ScreenHeight = Tulestaill*MaxScreenRow; // 660px height
-
-
-     Thread gameThread;
-     Image img = null;
-     int x,y;
-     TerrainManage terrainManage = new TerrainManage(this);
-     //CreatureManage creatureManage = new CreatureManage(this);
-
-
-    public GamePanel() throws IOException {
-         this.setPreferredSize(new Dimension(ScreenWidth,ScreenHeight));
-         this.setBackground(Color.black);
-         this.setDoubleBuffered(true);
+    final int Tulestaill = Tules * scale; // 60px dans le plateau width
+    final int TullesLarg = Tulestaill + 10; // 70px height
+    final int MaxScreenCol = 14;
+    final int MaxScreenRow = 11;
+    final int ScreenWidth = Tulestaill * MaxScreenCol; // 960px width
+    final int ScreenHeight = Tulestaill * MaxScreenRow; // 660px height
+    Point mouse = new Point();
 
 
 
+
+
+    TerrainManage terrainManage = new TerrainManage(this);
+    Thread gameThread;
+    Image Island;
+    Hexagon hexagone = new Hexagon(0,0);
+
+
+    int x, y;
+    Boolean Status = false;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(ScreenWidth, ScreenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                mouse = e.getPoint();
+                e.getComponent().repaint();
+
+            }
+        });
 
 
     }
+
     public void startGameThread() {
         gameThread = new Thread();
         gameThread.start();
+
     }
+
+
+
+
     @Override
     public void run() {
 
     }
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
 
+    public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        Graphics2D g3 = (Graphics2D) g;
-        terrainManage.draw(g2);
-        //creatureManage.draw(g3);
+        //int xi = 118;
+        //int yi = 261;
+
+        //Hexagone hexagone = new Hexagone(xi, yi);
+        super.paintComponent(g2);
+        try {
+            Island = ImageIO.read(getClass().getResourceAsStream("/bgisland.jpeg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //int[] xp  = {30,40,50,40,30,20};
+        //int[] yp  = {50,50,40,20,20,40};
+        g2.setColor(Color.RED);
+        //g2.drawPolygon(xp,yp,xp.length);
+        g2.drawImage(Island, 0, 0, ScreenWidth, ScreenHeight, null);
+        hexagone.painthexagon(g2);
+        g2.drawString(
+                "contains(" + (mouse.x) + ", " + (mouse.y) + ") is "
+                        + hexagone.contains(209, 19), 10, 20);
+
+
+
+        //g2.drawPolygon(hexagone);
+        //hexagone.painthexagon(g2,x,y);
+        //terrainManage.draw(g2);
+        //hexagone.drawHexagon(g2, 118, 261);
+
         g2.dispose();
+
+
+
+
 
     }
 
+    public  void event ( int x , int y) {
+        if(hexagone.contains(x,y) == false   ){
+            System.out.println("true");
+
+        }else System.out.println("False");
+    }
 
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+        System.out.println("YESSS");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+        System.out.println(e.getX()+"  "+e.getY() );
+        event(e.getX(),e.getY());
+
+    }
 }
---
+++ src/Hexagon.java
@@ -0,0 +1,99 @@
+import java.awt.*;
+import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Hexagon extends Polygon {
+    int xi;
+    int yi;
+    public Hexagon(int xi, int yi) {
+        this.xi = xi;
+        this.yi = yi;
+        this.addPoint(this.xi, this.yi);
+        this.addPoint(this.xi+30, this.yi-17);
+        this.addPoint(this.xi+60, this.yi+1);
+        this.addPoint(this.xi+60, this.yi+32);
+        this.addPoint(this.xi+30, this.yi+47);
+        this.addPoint(this.xi, this.yi+31);
+
+
+
+
+    }
+
+
+
+
+    /*public  void hexagoneMap ()
+    {
+
+
+        int [] xP = {xi,xi+30,xi+60,xi+60,xi+30,xi};
+        int [] yP = {yi,yi-17,yi+1,yi+32,yi+47,yi+31};
+      for(int i =0; i<6;i++){
+          hexagone.addPoint(xP[i],yP[i]);
+
+
+      }
+*/
+
+    public List readconfigMap() {
+        List<List<Integer>> lines = new ArrayList<List<Integer>>();
+
+        try {
+            Scanner scanner = new Scanner(new File("C:\\Users\\Medmat\\Desktop\\mapdata.txt"));
+
+            while (scanner.hasNextLine()) {
+                // List line = new List();
+                List<Integer> line = new ArrayList<Integer>();
+                String data[] = scanner.nextLine().split(",");
+                line.add(Integer.parseInt(data[0]));
+                line.add(Integer.parseInt(data[1]));
+                line.add(Integer.parseInt(data[2]));
+                lines.add(line);
+
+
+            }
+            lines.forEach(System.out::println);
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return  lines;
+    }
+
+
+
+
+    /* public void drawHexagon(Graphics2D g, int xi, int yi){
+         int[] xP = {xi, xi + 30, xi + 60, xi + 60, xi + 30, xi};
+         int[] yP = {yi, yi - 17, yi + 1, yi + 32, yi + 47, yi + 31};
+         g.drawPolygon(xP, yP, xP.length);
+
+     }*/
+    public void  painthexagon( Graphics2D g2 ) {
+        List<List<Integer>> linesOfMap = readconfigMap();
+        int idx_reglage = 0;
+        for(List<Integer> line : linesOfMap){
+            idx_reglage++;
+            int xi=line.get(1);
+            int yi=line.get(2);
+            for(int i=0; i<line.get(0); i++ ){
+                System.out.println("i:" +i+" ,xi: "+xi+", yi: "+yi);
+
+                g2.drawPolygon(new Hexagon(xi, yi));
+
+
+                if(i >= line.get(0)/2 || (i<line.get(0)-2 && idx_reglage>7 && i >= line.get(0)/2)) xi-=4;
+                xi+=62;
+            }
+        }
+
+
+
+
+    }
+}
