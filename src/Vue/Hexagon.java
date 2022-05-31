2d78b441fdba61992acd812bd15dc9bb1b0b78ec
Medmat0 <66081986+Medmat0@users.noreply.github.com>
2022-05-31 15:24:03 +0200

    Update Hexagon.java
    new update() drawing

--- src/Vue/Hexagon.java
+++ src/Vue/Hexagon.java
@@ -1,6 +1,7 @@
 package Vue;
 
 import java.awt.*;
+import java.awt.event.MouseMotionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -8,6 +9,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Hexagon extends Polygon {
+
+    Creature creature;
+    Terrain terrain = new Terrain();
+    Tuile tuile;
+    Image imageHexagone = null;
+
     int xi;
     int yi;
     int xdraw;
@@ -23,26 +30,24 @@ public class Hexagon extends Polygon {
         this.addPoint(this.xi + 60, this.yi + 32);
         this.addPoint(this.xi + 30, this.yi + 47);
         this.addPoint(this.xi, this.yi + 31);
+        terrain.Maptuile();
+
+
+
+
 
     }
 
     List<Hexagon> listhex = new ArrayList<>();
 
 
-    /*public  void hexagoneMap ()
-    {
-        int [] xP = {xi,xi+30,xi+60,xi+60,xi+30,xi};
-        int [] yP = {yi,yi-17,yi+1,yi+32,yi+47,yi+31};
-      for(int i =0; i<6;i++){
-          hexagone.addPoint(xP[i],yP[i]);
-      }
-*/
+
 
     public List readconfigMap() {
         List<List<Integer>> lines = new ArrayList<List<Integer>>();
 
         try {
-            Scanner scanner = new Scanner(new File("C:\\Users\\dabli\\OneDrive\\Bureau\\mapdata.txt"));
+            Scanner scanner = new Scanner(new File("C:\\Users\\Medmat\\Desktop\\mapdata.txt"));
 
             while (scanner.hasNextLine()) {
                 // List line = new List();
@@ -71,8 +76,10 @@ public class Hexagon extends Polygon {
             int xi = line.get(1);
             int yi = line.get(2);
             for (int i = 0; i < line.get(0); i++) {
-                System.out.println("i:" + i + " ,xi: " + xi + ", yi: " + yi);
+                //System.out.println("i:" + i + " ,xi: " + xi + ", yi: " + yi);
                 listhex.add(new Hexagon(xi, yi));
+
+
                 if (i >= line.get(0) / 2 || (i < line.get(0) - 2 && idx_reglage > 7 && i >= line.get(0) / 2)) xi -= 4;
                 xi += 62;
             }
@@ -91,10 +98,26 @@ public class Hexagon extends Polygon {
 
         }
     }
+    public boolean ilecentre( int indice1){
+
+        int indicehexagon[] = {31,32,33,34,41,42,43,44,45,51,52,53,54,55,56,57,58,63,64,65,67,68,69,74,75,76,77,78,79,80,81,87,88,89,90,91,98,99,100,101};
+        for (int i = 0; i < 40; i++) {
+            if( indice1 == indicehexagon[i]){
+
+
+                return true;
+            }
+
+        }
+        return false;
+
+
+
+    }
 
     public void checkxy(int x, int y) {
         for (Hexagon elem : listhex) {
-            if (elem.contains(x, y) == true) {
+            if (elem.contains(x, y+17) == true) {
                 xdraw = elem.xpoints[0];
                 ydraw = elem.ypoints[0];
                 elemhexa = listhex.indexOf(elem);
@@ -108,21 +131,17 @@ public class Hexagon extends Polygon {
 
 
     public void drawHexagon(Graphics2D g) {
-        List<List<Integer>> linesOfMap = readconfigMap();
-        int idx_reglage = 0;
-        for (List<Integer> line : linesOfMap) {
-            idx_reglage++;
-            int xi = line.get(1);
-            int yi = line.get(2);
-            for (int i = 0; i < line.get(0); i++) {
-                //g.drawPolygon(new Hexagon(xi, yi));
-                if (i >= line.get(0) / 2 || (i < line.get(0) - 2 && idx_reglage > 7 && i >= line.get(0) / 2)) xi -= 4;
-                xi += 62;
-            }
 
+        int i = 0;
+          for(Hexagon elem: listhex){
+              if(ilecentre(listhex.indexOf(elem))){
+                  g.drawImage(terrain.tabelImag[i],elem.xpoints[0],elem.ypoints[0]-17,58,68,null);
+                  i++;
+              }
+          }
 
-        }
 
 
     }
 }
+
